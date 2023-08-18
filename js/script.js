49cfa9ea8ecf4b6cb38a3b9737fb2b181012325f
Raj Singh <rajsinghast03@gmail.com>
2023-08-18 07:35:53 +0530

    Add Feature

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const searchButton = document.querySelector(".search__button");
 const city = document.querySelector(".city");
 const country = document.querySelector(".country");
-const temperature = document.querySelector(".item");
+const temperature = document.querySelector(".tc");
 const aqi = document.querySelector(".aqi");
 const aqi__info = document.querySelector(".aqi__info");
 const visibility = document.querySelector(".vis");
@@ -13,9 +13,15 @@ const sunset = document.querySelector(".dusk");
 const windSpeed = document.querySelector(".wind__speed");
 const windDir = document.querySelector(".direction");
 const uvIndex = document.querySelector(".index");
-
+const weatherNowIcon = document.querySelector(".w_now");
+const rainNowIcon = document.querySelector(".w_rain");
 const currWeather = document.querySelector(".weather__curr");
 const rainChances = document.querySelector(".rain__per");
+
+function rotateWindIcon(degrees) {
+  const icon = document.querySelector(".compass");
+  icon.style.transform = `rotate(${degrees}deg)`;
+}
 
 const setTime = function (curr) {
   const currTime = curr.slice(-5);
@@ -25,9 +31,6 @@ function getDayName(curr) {
   return date.toLocaleDateString({ weekday: "long" });
 }
 console.log(getDayName("2023-08-17"));
-
-var dateStr = "05/23/2014";
-var day = getDayName(dateStr, "nl-NL");
 
 const getHumidInfo = function () {};
 
@@ -45,31 +48,60 @@ const getAqiInfo = function (index) {
   }
 };
 
+const progressBar = document.querySelector(".bar");
+
+const progress = function (progressValue) {
+  const dg = `${progressValue * 4}` * 4.5;
+  progressBar.style.rotate = `${-135 + dg}deg`;
+};
+
+progress(8);
+
 const getWeatherDetails = async function (inputLocation) {
-  if (inputLocation == "") return;
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=3bb2dcb7567d448bad5110206231008&q=${inputLocation}&aqi=yes`
-  );
-  const data = await response.json();
-  city.textContent = `${data.location.name},`;
-  country.textContent = data.location.country;
-  aqi.textContent = data.current.air_quality["gb-defra-index"];
-  getAqiInfo(data.current.air_quality["gb-defra-index"]);
-  visibility.innerHTML = `${data.current.vis_km} <span class="km">km</span>`;
-  humidity.textContent = `${data.current.humidity}%`;
-  currWeather.textContent = data.current.condition.text;
-  windDir.textContent = data.current.wind_dir;
-  uvIndex.textContent = data.current.uv;
-  windSpeed.innerHTML = `${data.current.wind_kph} <span class="km">km/h</span>`;
-  sunrise.textContent = data.forecast.forecastday[0].astro.sunrise;
-  sunset.textContent = data.forecast.forecastday[0].astro.sunset;
+  try {
+    if (inputLocation == "") return;
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=3bb2dcb7567d448bad5110206231008&q=${inputLocation}&aqi=yes`
+    );
+    const data = await response.json();
+    if (data.location === undefined) {
+      alert("Invalid City Name!");
+      throw new Error("Invalid City Name");
+    }
+    //setting icon according to weather
+    const code = data.current.condition.icon.slice(-7, -4);
+    console.log(code);
+    const path = `weather_icons/${
+      data.current.is_day ? "day" : "night"
+    }/${code}.png`;
+    weatherNowIcon.src = path;
+    temperature.textContent = Math.round(data.current.temp_c);
+    city.textContent = `${data.location.name},`;
+    country.textContent = data.location.country;
+    aqi.textContent = data.current.air_quality["gb-defra-index"];
+    getAqiInfo(data.current.air_quality["gb-defra-index"]);
+    visibility.innerHTML = `${data.current.vis_km} <span class="km">km</span>`;
+    humidity.textContent = `${data.current.humidity}%`;
+    currWeather.textContent = data.current.condition.text;
+    windDir.textContent = data.current.wind_dir;
+    progress(data.current.uv);
+    uvIndex.textContent = data.current.uv;
+    windSpeed.innerHTML = `${data.current.wind_kph} <span class="km">km/h</span>`;
+    const deg = -45 + data.current.wind_degree;
 
-  const hourlength = data.forecast.forecastday[0].hour.length;
+    rotateWindIcon(deg);
+    sunrise.textContent = data.forecast.forecastday[0].astro.sunrise;
+    sunset.textContent = data.forecast.forecastday[0].astro.sunset;
 
-  rainChances.innerHTML = `${
-    data.forecast.forecastday[0].hour[hourlength - 1].chance_of_rain
-  }% - Rain`;
-  console.log(data);
+    const hourlength = data.forecast.forecastday[0].hour.length;
+
+    rainChances.innerHTML = `${
+      data.forecast.forecastday[0].hour[hourlength - 1].chance_of_rain
+    }% - Rain`;
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 searchButton.addEventListener("click", function (e) {
